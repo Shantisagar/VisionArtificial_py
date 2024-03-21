144c5c46a3476d376ee0ece2f7ff9b8aeed241d9
AgustinMadygraf <agustin.mtto.madygraf>
2024-03-21 12:10:15 -0300

    Add logging functionality and error handling to main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
-#VisionArtificial\main.py
+# VisionArtificial\main.py
 import tkinter as tk
 from video_stream import VideoStreamApp
 import sys
 from screeninfo import get_monitors
 import json
 import os
+import logging
+from logs.config_logger import configurar_logging
 
-for monitor in get_monitors():
-    print(f"Monitor {monitor.name}: {monitor.width}x{monitor.height}")
+# Configuración del logger
+logger = configurar_logging()
 
-#crear funcion que tome los valores de config.json y los almacene en variables
 def leer_configuracion():
+    """
+    Lee la configuración desde el archivo 'config.json' y devuelve un diccionario con los valores configurados.
+    Si el archivo no existe, se crea uno con valores predeterminados.
+    """
     ruta_configuracion = 'src/config.json'
     configuracion_predeterminada = {
         "grados_rotacion_default": 0,
@@ -21,47 +26,54 @@ def leer_configuracion():
         "ubicacion_default": "C:/AppServ/www/VisionArtificial/tests/calibracion_deteccion_papel.jpg"
     }
 
-    # Verificar si el archivo de configuración existe
-    if not os.path.exists(ruta_configuracion):
-        # Si no existe, crear el archivo con la configuración predeterminada
-        with open(ruta_configuracion, 'w') as archivo:
-            json.dump(configuracion_predeterminada, archivo, indent=4)
-        print("Archivo de configuración creado con valores predeterminados.")
+    try:
+        if not os.path.exists(ruta_configuracion):
+            with open(ruta_configuracion, 'w') as archivo:
+                json.dump(configuracion_predeterminada, archivo, indent=4)
+            logger.info("Archivo de configuración creado con valores predeterminados.")
 
-    # Ahora, leer la configuración desde el archivo
-    with open(ruta_configuracion, 'r') as archivo:
-        datos = json.load(archivo)
+        with open(ruta_configuracion, 'r') as archivo:
+            datos = json.load(archivo)
+    except Exception as e:
+        logger.error(f"Error al leer o crear el archivo de configuración: {e}")
+        sys.exit(1)
     
     return datos
 
-
 def manejar_menu(config):
-    opcion = input("Seleccione una opción:\n0 - Testing\n1 - RTSP\n2 - HTTP (No disponible aún)\nOpción: ") or "0"
-    if opcion == "0":
-        print("Modo de calibración de reconocimiento de imagen activado.")
-        # Usa el valor de la configuración para la ubicación de calibración
-        return config["ubicacion_default"]
-    elif opcion == "1":
-        print("Modo de transmisión RTSP activado.")
-        # Usa el valor de la configuración para la URL RTSP
-        return config["rtsp_url_default"]
-    elif opcion == "2":
-        print("HTTP no está disponible aún.")
-        return None
-    else:
-        print("Opción no válida.")
+    """
+    Maneja el menú de opciones de usuario y devuelve la URL del video o ruta de la imagen seleccionada.
+    """
+    try:
+        opcion = input("Seleccione una opción:\n0 - Testing\n1 - RTSP\n2 - HTTP (No disponible aún)\nOpción: ") or "0"
+        if opcion == "0":
+            logger.info("Modo de calibración de reconocimiento de imagen activado.")
+            return config["ubicacion_default"]
+        elif opcion == "1":
+            logger.info("Modo de transmisión RTSP activado.")
+            return config["rtsp_url_default"]
+        elif opcion == "2":
+            logger.info("HTTP no está disponible aún.")
+            return None
+        else:
+            logger.error("Opción no válida.")
+            sys.exit(1)
+    except Exception as e:
+        logger.error(f"Error al manejar el menú de opciones: {e}")
         sys.exit(1)
 
-
 if __name__ == "__main__":
-    config = leer_configuracion()
-    # Ahora, extrae cada valor de configuración que necesitas
-    grados_rotacion         = float(input(f'Ingrese los grados de rotación (en sentido antihorario, "   {config["grados_rotacion_default"]} " por defecto): ') or config["grados_rotacion_default"])
-    altura                  = float(input(f'Ingrese la altura para corregir el eje vertical, "          {config["altura_default"]}          " por defecto): ') or config["altura_default"])
-    perspectiva_default     = float(input(f'Ingrese la altura para corregir la perspectiva, "           {config["perspectiva_default"]}     " por defecto): ') or config["perspectiva_default"])
-    altura2                 = float(input(f'Ingrese la altura para corregir el eje vertical, "          {config["altura2_default"]}         " por defecto): ') or config["altura2_default"])
-    default_video_url = manejar_menu(config)
-    root = tk.Tk()
-    app = VideoStreamApp(root, default_video_url, grados_rotacion, altura,perspectiva_default)
-    app.run()
+    try:
+        config = leer_configuracion()
+        grados_rotacion = float(input(f'Ingrese los grados de rotación (en sentido antihorario, "   {config["grados_rotacion_default"]} " por defecto): ') or config["grados_rotacion_default"])
+        altura = float(input(f'Ingrese la altura para corregir el eje vertical, "          {config["altura_default"]}          " por defecto): ') or config["altura_default"])
+        perspectiva_default = float(input(f'Ingrese la altura para corregir la perspectiva, "           {config["perspectiva_default"]}     " por defecto): ') or config["perspectiva_default"])
+        altura2 = float(input(f'Ingrese la altura para corregir el eje vertical, "          {config["altura2_default"]}         " por defecto): ') or config["altura2_default"])
+        default_video_url = manejar_menu(config)
 
+        root = tk.Tk()
+        app = VideoStreamApp(root, default_video_url, grados_rotacion, altura, perspectiva_default)
+        app.run()
+    except Exception as e:
+        logger.error(f"Error al iniciar la aplicación: {e}")
+        sys.exit(1)
